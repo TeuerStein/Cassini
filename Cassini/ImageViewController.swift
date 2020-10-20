4af8cc2b3ba29cb398ceca9290854e54bf1047d9
TeuerStein <xenkai123456789@gmail.com>
2020-10-20 15:14:33 +0200

    Was added a ImageViewController and new View Controller with Image View

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by OLEKSANDR SOKOLOV on 20.10.2020.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
